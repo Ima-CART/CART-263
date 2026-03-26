15cdc2498f867f3e3950b63d70bd4d7d77ca3f77
Ima-CART <Sandra_Williams93@hotmail.com>
2026-03-26 12:55:50 -0400

    Start new task

--- CART263-TASKII-IMA/task-js/script.js
+++ CART263-TASKII-IMA/task-js/script.js
@@ -2,16 +2,16 @@ window.onload = go();
 
 function go() {
 
- /***** GIVEN :: ******************************/
-/* function to go through the buttons and select which canvas to activate */
+  /***** GIVEN :: ******************************/
+  /* function to go through the buttons and select which canvas to activate */
   function mainMenu() {
     document.querySelectorAll(".ani-title").forEach(function (el) {
       el.addEventListener("click", function (e) {
         //reset the canvases with button every click
-        document.querySelector("#draw-a").innerHTML=''
-        document.querySelector("#draw-b").innerHTML=''
-        document.querySelector("#draw-c").innerHTML=''
-        document.querySelector("#draw-d").innerHTML=''
+        document.querySelector("#draw-a").innerHTML = ''
+        document.querySelector("#draw-b").innerHTML = ''
+        document.querySelector("#draw-c").innerHTML = ''
+        document.querySelector("#draw-d").innerHTML = ''
 
         if (this.id === "one") {
           goOne();
@@ -28,107 +28,112 @@ function go() {
     }); //for each
   }
 
-//call 
+  //call 
   mainMenu();
- /************************************************************** */
-/* first canvas function*/ 
- /************************************************************** */
-  function goOne(){
-  console.log("in one")
-   //PART 1: create an array called `cat_array` to hold 10 `cats`
-   
-   //PART 2: add 10 `cats` to to the `cat_array` using the `createCat()`-
-   // Each `cat` should be 50px by 50px  
-   // cats 1-5 should be have a y value of 50 and x going from 10 - 210 
-   //cats  6-10 should have a y value of 200 and x going from 10 - 210 
-   // the objects all need to be in the div with id `draw-a`
-
-   //PART 3: use a setTimeout to change the src property of the first 5 images to be cat_4_b.svg in 10 seconds.
-   //Use another setTimeout to change the src property of the last 5 images to be cat_4_c.svg in 15 seconds
-
-
-   //PART 4: comment out ALL of PART 3 and use a setInterval to every 2 seconds to starting with the first image:
-   // set the opacity of the next cat image to 0 
-   // ending with the 10th image (will be 20 seconds later)
-   // After the last image has had its opacity set to 0 clear the setInterval
-
-   /***** GIVEN :: ******************************/
-  // function that generates a cat image in a div 
-    function createCat(parentDiv,x,y,w,h){
-        let outer_img = document.createElement("div");
-        let img = document.createElement("img")
-        outer_img.classList.add("out-cont")
-        outer_img.appendChild(img)
-        img.src = "task-assets/cat_4.svg"
-        img.classList.add("img-class-cat");
-        outer_img.style.height = h+"px"
-        outer_img.style.width = w+"px"
-        outer_img.style.left = x+"px"
-        outer_img.style.top = y+"px"
-        outer_img.style.opacity = "1";
-        parentDiv.appendChild(outer_img)
-        return outer_img;
-
-    }
- }
-
- /************************************************************** */
- /* second canvas function*/ 
   /************************************************************** */
- function goTwo(){
- console.log("in two")
-  //PART 0: create a variable called `numDogs` to hold the value `2` - 
-  // this will be the number of dogs we start with
+  /* first canvas function*/
+  /************************************************************** */
+  function goOne() {
+    console.log("in one")
+    //PART 1: create an array called `cat_array` to hold 10 `cats`  
+    let cat_array = new array(10);
+    //PART 2: add 10 `cats` to the `cat_array` using the `createCat()`-
+    // Each `cat` should be 50px by 50px  
+    // cats 1-5 should be have a y value of 50 and x going from 10 - 210 
+    //cats  6-10 should have a y value of 200 and x going from 10 - 210 
+    // the objects all need to be in the div with id `draw-a`
 
-  //PART 1: create an array called `dog_array` to hold 2 `dogs`
-   
-   //PART 2: add 2 `dogs` to to the `dog_array` using the `createDog()`-
-   //Each `dog` should be 100px by 100px  
-   // dog 1 should be have a y value of 50 and x of 110 and an id of "a_dog"
-   //dog 2 should have a y value of 50 and x of 220 and an id of "b_dog"
-   // the objects all need to be in the div with id `draw-b`
+    for (let i = 0; i < 10; i++) {
+      cat_array.push(createCat())
+    }
+    //PART 3: use a setTimeout to change the src property of the first 5 images to be cat_4_b.svg in 10 seconds.
+    //Use another setTimeout to change the src property of the last 5 images to be cat_4_c.svg in 15 seconds
 
-   //PART 3: - the createDog() has an  event listener for `click` on the `img` variable:
-   // Please complete the `checkCollision` call back function to have the following functionality:
-   //A: use a `setTimeout` to change the img `src` to be `dog_c.svg` 2 seconds after the image is clicked
-   //B: use another `setTimeout` to run after 3 seconds to create another dog 
-   // at a random position between 0 and 200 on the x and 0 and 400 on the y
-   // -> then add this new dog to the `dog_array` and ensure that it has an id of `dog_${numDogs}`
-    
-      /***** GIVEN :: ******************************/
-    // function that generates a dog image in a div 
-    function createDog(parentDiv,x,y,w,h,id){
-        let outer_img = document.createElement("div");
-        let img = document.createElement("img")
-        outer_img.classList.add("out-cont")
-        outer_img.appendChild(img)
-        img.src = "task-assets/dog_a.svg"
-        img.classList.add("img-class-dog");
-        outer_img.style.height = h+"px"
-        outer_img.style.width = w+"px"
-        outer_img.style.left = x+"px"
-        outer_img.style.top = y+"px"
-        outer_img.style.opacity = "1";
-        img.addEventListener("click",checkCollision)
-        outer_img.id = id
-        parentDiv.appendChild(outer_img)
-        return outer_img;
+
+    //PART 4: comment out ALL of PART 3 and use a setInterval to every 2 seconds to starting with the first image:
+    // set the opacity of the next cat image to 0 
+    // ending with the 10th image (will be 20 seconds later)
+    // After the last image has had its opacity set to 0 clear the setInterval
+
+    /***** GIVEN :: ******************************/
+    // function that generates a cat image in a div 
+    function createCat(parentDiv, x, y, w, h) {
+      let outer_img = document.createElement("div");
+      let img = document.createElement("img")
+      outer_img.classList.add("out-cont")
+      outer_img.appendChild(img)
+      img.src = "task-assets/cat_4.svg"
+      img.classList.add("img-class-cat");
+      outer_img.style.height = h + "px"
+      outer_img.style.width = w + "px"
+      outer_img.style.left = x + "px"
+      outer_img.style.top = y + "px"
+      outer_img.style.opacity = "1";
+      parentDiv.appendChild(outer_img)
+      return outer_img;
 
     }
-       /***** GIVEN :: ******************************/
+  }
+
+  /************************************************************** */
+  /* second canvas function*/
+  /************************************************************** */
+  function goTwo() {
+    console.log("in two")
+    //PART 0: create a variable called `numDogs` to hold the value `2` - 
+    // this will be the number of dogs we start with
+    let numDogs = 2
+
+    //PART 1: create an array called `dog_array` to hold 2 `dogs`
+    let dog_array = []
+
+    //PART 2: add 2 `dogs` to to the `dog_array` using the `createDog()`-
+    //Each `dog` should be 100px by 100px  
+    // dog 1 should be have a y value of 50 and x of 110 and an id of "a_dog"
+    //dog 2 should have a y value of 50 and x of 220 and an id of "b_dog"
+    // the objects all need to be in the div with id `draw-b`
+
+    //PART 3: - the createDog() has an  event listener for `click` on the `img` variable:
+    // Please complete the `checkCollision` call back function to have the following functionality:
+    //A: use a `setTimeout` to change the img `src` to be `dog_c.svg` 2 seconds after the image is clicked
+    //B: use another `setTimeout` to run after 3 seconds to create another dog 
+    // at a random position between 0 and 200 on the x and 0 and 400 on the y
+    // -> then add this new dog to the `dog_array` and ensure that it has an id of `dog_${numDogs}`
+
+    /***** GIVEN :: ******************************/
+    // function that generates a dog image in a div 
+    function createDog(parentDiv, x, y, w, h, id) {
+      let outer_img = document.createElement("div");
+      let img = document.createElement("img")
+      outer_img.classList.add("out-cont")
+      outer_img.appendChild(img)
+      img.src = "task-assets/dog_a.svg"
+      img.classList.add("img-class-dog");
+      outer_img.style.height = h + "px"
+      outer_img.style.width = w + "px"
+      outer_img.style.left = x + "px"
+      outer_img.style.top = y + "px"
+      outer_img.style.opacity = "1";
+      img.addEventListener("click", checkCollision)
+      outer_img.id = id
+      parentDiv.appendChild(outer_img)
+      return outer_img;
+
+    }
+    /***** GIVEN :: ******************************/
     //callback function 
-    function checkCollision(e){
+    function checkCollision(e) {
       numDogs++;
       let el = e.target
       console.log(el)
-   }
+    }
   }
 
- /************************************************************** */
-/* third canvas function*/ 
- /************************************************************** */
-  function goThree(){
-  console.log("in in three")
+  /************************************************************** */
+  /* third canvas function*/
+  /************************************************************** */
+  function goThree() {
+    console.log("in in three")
     //PART 1: create an a variable called `dog` to hold a `dog`
     //and a variable called `cat` to hold a `cat`
     //use the  `createDog()` and the `createCat()` respectively to assign 
@@ -143,8 +148,8 @@ function go() {
     //PART 2B:
     //use the checkEdgesDog() function (given) to ensure that the dog will switch direction when it is at the edge of the canvas
 
-     // PART 3A:
-     // animate the cat: within the `animate()` function, move the cat on the y axis using the
+    // PART 3A:
+    // animate the cat: within the `animate()` function, move the cat on the y axis using the
     // `catSpeedY` variable
 
     //PART 3B:
@@ -154,8 +159,8 @@ function go() {
     //(HINT::) use thecheckEdgesDog() function as inspiration :)
     //3: call this function ...
 
-  /***** GIVEN :: ******************************/
-  // function that generates a dog image in a div
+    /***** GIVEN :: ******************************/
+    // function that generates a dog image in a div
     function createDog(parentDiv, x, y, w, h, id) {
       let outer_img = document.createElement("div");
       let img = document.createElement("img");
@@ -174,7 +179,7 @@ function go() {
     }
 
     /***** GIVEN :: ******************************/
-  // function that generates a cat image in a div
+    // function that generates a cat image in a div
     function createCat(parentDiv, x, y, w, h) {
       let outer = document.createElement("div");
       let img = document.createElement("img");
@@ -202,13 +207,13 @@ function go() {
     /***** GIVEN :: ******************************/
     //the function that is called every frame
     function animate() {
-       
+
 
       window.requestAnimationFrame(animate);
     }
-     /***** GIVEN :: ******************************/
-     // the function to check if the dog is at the edge of the canvas and needs
-     //to switch direction
+    /***** GIVEN :: ******************************/
+    // the function to check if the dog is at the edge of the canvas and needs
+    //to switch direction
     function checkEdgesDog() {
       let boundaries = document.querySelector("#draw-c").getBoundingClientRect();
       if (
@@ -216,23 +221,23 @@ function go() {
         parseInt(dog.style.left) <= 0
       )
         dogSpeedX *= -1;
-        //flip the image using scale...
-        if(dogSpeedX>0){
-          dog.style.transform='scale(1,1)';
-        }
-        else{
-          dog.style.transform='scale(-1,1)';
-        }
-        
-      }//checkEdgesDog
- }
-/* fourth canvas function*/ 
-  function goFour(){
-  console.log("in in four")
-  // FREESTYLE USING WHATEVER (your own shapes/images... text...)
-  // use window.requestAnimationFrame() to create your own animation
+      //flip the image using scale...
+      if (dogSpeedX > 0) {
+        dog.style.transform = 'scale(1,1)';
+      }
+      else {
+        dog.style.transform = 'scale(-1,1)';
+      }
+
+    }//checkEdgesDog
+  }
+  /* fourth canvas function*/
+  function goFour() {
+    console.log("in in four")
+    // FREESTYLE USING WHATEVER (your own shapes/images... text...)
+    // use window.requestAnimationFrame() to create your own animation
 
 
-    
- }
+
+  }
 }
