d4ce6a63e01ac9af91d1bde6821ba20b1edc92a6
Ima-CART <Sandra_Williams93@hotmail.com>
2026-04-08 01:04:19 -0400

    Working on JS
    Working on JSON files

---
+++ FETCH-2026-START/js/jsonFetch.js
@@ -0,0 +1,53 @@
+window.onload = goApp;
+
+async function goApp() {
+    console.log("hello fetch")
+
+    try {
+
+        let response = await fetch('../files/tests.json'); //response
+        let parsedResultJS = await response.json();
+        console.log(parsedResultJS)
+
+        // let textResult = await response.text();
+        // console.log(textResult)
+    }
+    catch (err) {
+
+        console.log(err)
+
+        displayResults()
+    }
+
+
+    function displayResults(parsedResultJS) {
+        for (let i = 0; i < parsedResultJS.length; i++) {
+            console.log(parsedResultJS)
+
+            //the object
+            let donutObj = parsedResultJS[i];
+            //container
+            let containerDiv = document.createElement("div");
+            containerDiv.classList.add("donutItem");
+            document.querySelector("#output").appendChild(containerDiv);
+
+            let title = document.createElement("h3");
+            title.textContent = donutObj.name;
+            containerDiv.appendChild(title)
+
+            //access the image
+            let donutImage = document.createElement("img");
+            donutImage.src = donutObj.image;
+            containerDiv.appendChild(donutImage)
+
+
+        }
+
+
+    }
+
+
+
+
+}
+
